94cedf4abf2bb2c5b3371899a7e0420a91a5f413
Charles Zoeller <charles.zoeller1@gmail.com>
2021-02-15 17:33:41 -0700

    added initial inquirer questions

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "Enter a description for your project: ",
+        name: "description",
+      },
+      {
+        type: "input",
+        message: "How can users install your project?",
+        default: "npm i",
+        name: "install",
+      },
+      {
+        type: "input",
+        message: "How is your project used?",
+        name: "usage",
+      },
+      {
+        type: "input",
+        message: "How can others contribute to your project?",
+        name: "contribution",
+      },
+      {
+        type: "input",
+        message: "How do users test your project?",
+        name: "tests",
+      },
+      {
+        type: "list",
+        message: "What is your project's License?",
+        choices: [],
+        name: "license",
+      },
+      {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "gitName",
+      },
+      {
+        type: "input",
+        message: "What is the your email address?",
+        name: "email",
+      },
+    ])
+    .then(function (response) {});
+}
 
 // Function call to initialize app
 init();
