e601e62812f002c4e7e32727333f8926a4063416
Charles Zoeller <charles.zoeller1@gmail.com>
2021-02-15 18:06:02 -0700

    started creating readme text

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ function writeToFile(fileName, data) {
   );
 }
 
+function contribution(inc) {
+  if (inc) {
+    return "[Guidelines](https://www.contributor-covenant.org/version/2/0/code_of_conduct/)";
+  }
+  return "Contact me for further information.";
+}
+
 // TODO: Create a function to initialize app
 function init() {
   inquirer
@@ -35,16 +42,18 @@ function init() {
       {
         type: "input",
         message: "How is your project used?",
+        default: "node index.js",
         name: "usage",
       },
       {
-        type: "input",
-        message: "How can others contribute to your project?",
+        type: "confirm",
+        message: "Do you want to include contributing guidelines?",
         name: "contribution",
       },
       {
         type: "input",
         message: "How do users test your project?",
+        default: "npm run test",
         name: "tests",
       },
       {
@@ -64,7 +73,53 @@ function init() {
         name: "email",
       },
     ])
-    .then(function (response) {});
+    .then(function (response) {
+      console.log(response);
+      //   Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+      //  license badge  ![APM](https://img.shields.io/apm/l/vim-mode)
+      let text = `
+      # ${response.title}
+      ## Description
+
+      ${response.description}
+
+      ## Table of Contents
+      [Installation](#installation)
+      [Usage](#usage)
+      [License](#license)
+      [Contributing](#contributing)
+      [Tests](#test)
+      [Questions](#questions)
+
+      ## Installation
+
+      ${response.install}
+
+      ## Usage
+
+      ${response.usage}
+
+      ## License
+
+      ${response.license}
+
+      ## Contributing
+
+      ${contribution(response.contribution)}
+
+      ## Tests
+
+      ${response.tests}
+
+      ## Questions
+
+      Contact me:
+      [Github](https://github.com/${response.gitName})
+
+      ${response.email}
+      `;
+      console.log(text);
+    });
 }
 
 // Function call to initialize app
